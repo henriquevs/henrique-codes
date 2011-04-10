5055c7bbdebd77b5dcd1ce8d4e657c22abc82179
henriquevs.br@gmail.com <henriquevs.br@gmail.com@e12c9158-6e02-11e0-5a6a-18029df63fb3>
2011-04-10 06:10:53 +0000

    Versão final do programa; indentações e comentários feitos corretamente; o programa roda perfeitamente os testes abertos do SuSy.

--- trunk/mc202/lab03/lab03_ra105063.c
+++ trunk/mc202/lab03/lab03_ra105063.c
@@ -1,4 +1,7 @@
-/* Preciso terminar este lab */
+/*  UNICAMP - Universidade Estadual de Campinas
+    MC202 - Estruturas de Dados
+    Aluno: Henrique Vicente Souza - RA: 105063 - Turma D
+    Laboratorio 03 - Banco Utopia */
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -53,7 +56,7 @@ Caixa *cria_caixas(int n){
          aux=inicio; /* Faz a ligacao do primeiro elemento da "lista" de caixas com um possível segundo elemento */
          p=p->next;
       }
-      else{ /* Vai preenchendo a sequencia de caixas na forma de lista ligada; notar que a referencia ao inicio desta lista nao se perde */
+      else{ /* Vai preenchendo a sequencia de caixas na forma de lista ligada; notar que a referencia ao inicio da lista nao se perde */
          p=(Caixa*)malloc(sizeof(Caixa));
          teste_memoria_caixa(p); /* Verifica */
          p->numero_do_caixa=i+1; /* Este numero representa o servico do caixa */
@@ -101,17 +104,16 @@ Caixa *insere(char nome[], int servico, int prioridade, Caixa *inicio){
       if(prioridade == 1){ /* Cliente especial */
          if(procura->inicio_especial==NULL || procura->fim_especial==NULL){ /* Fila de clientes especiais vazia */
             procura->inicio_especial=q;
-            procura->fim_especial=q; /* Atualia o final da fila */
+            procura->fim_especial=q; /* Atualiza o final da fila */
             return procura;
          }
          else{
             procura->fim_especial->prox=q;
-            procura->fim_especial=q; /* Atualia o final da fila */
+            procura->fim_especial=q; /* Atualiza o final da fila */
             return procura;
          }
       }
       else if(prioridade == 0){ /* Cliente normal */
-      
          if(procura->inicio_normal==NULL || procura->fim_normal==NULL){ /* Fila de clientes normais vazia */
             procura->inicio_normal=q;
             procura->fim_normal=q;
@@ -134,15 +136,11 @@ void imprime(Caixa *inicio){
    int flag1=0, flag2=0;
       
       /* Verifica se a fila de clientes especiais nao esta vazia */
-      if(p->inicio_especial!=NULL && p->fim_especial!=NULL){ /* Se entrarmos neste "if" eh porque a lista de clientes especiais nao eh vazia */
+      if(p->inicio_especial!=NULL && p->fim_especial!=NULL){ /* Se entrarmos neste "if" entao a lista de clientes especiais nao eh vazia */
          q=p->inicio_especial;
          r=q;
-         //printf("Visao geral dos clientes:\n");
          printf("Servico %d:\n", p->numero_do_caixa);
-         
          while(q!=NULL){ /* Anda ate o ultimo cliente especial */
-            
-            
             if(r->prox!=NULL && q->prox!=NULL){ /* Imprime ate o penultimo cliente especial */
                printf("%s->", q->nome);
                r=q;
@@ -154,20 +152,16 @@ void imprime(Caixa *inicio){
             }               
          }
          flag1=1; /* Indica que imprimimos todos os clientes especiais */
-      
       }
 
       /* Verifica se a fila de clientes normais nao esta vazia */
       if(p->inicio_normal!=NULL && p->fim_normal!=NULL){ /* Se entrarmos neste "if" eh porque a lista de clientes normais nao eh vazia */
-         
          /* Verifica se houve impressao de clientes especiais */
-         if(flag1 == 1){ /* Se entrarmos neste "if" eh porque houve impressao de clientes especiais */
+         if(flag1 == 1){ /* Se entrarmos neste "if" entao houve impressao de clientes especiais */
             q=p->inicio_normal;
             r=q;
             printf("->");
-            
             while(q!=NULL){
-               
                if(r->prox!=NULL && q->prox!=NULL){ /* Imprime ate o penultimo cliente normal */
                printf("%s->", q->nome);
                r=q;
@@ -179,18 +173,13 @@ void imprime(Caixa *inicio){
                }
             }
             flag2=1; /* Indica que imprimimos todos os clientes normais */
-         
          } 
-
          else{
-            if(flag1 == 0){ /* Se entrarmos neste "if eh porque NAO houve impressao de clientes especiais */
+            if(flag1 == 0){ /* Se entrarmos neste "if" entao NAO houve impressao de clientes especiais */
                q=p->inicio_normal;
                r=q;
-               //printf("Visao geral dos clientes:\n");
                printf("Servico %d:\n", p->numero_do_caixa);
-               
                while(q!=NULL){
-                  
                   if(r->prox!=NULL && q->prox!=NULL){ /* Imprime ate o penultimo cliente normal */
                   printf("%s->", q->nome);
                   r=q;
@@ -202,19 +191,15 @@ void imprime(Caixa *inicio){
                   }
                }
                flag2=1; /* Indica que imprimimos todos os clientes normais */
-            
             }
          }
-      
       }
-
       /* Se houve impressao de clientes, finalizamos a impressao com a quebra de linha */
       if(flag1==1 || flag2==1) printf("\n");
 }
 
 /* Impressao dos proximos a serem atendidos */
 void imprime_proximos(Caixa *balcao){
-   
    if(balcao->inicio_especial != NULL){ /* Verifica se a fila de clientes especiais esta vazia */
       printf("Servico %d:\n", balcao->numero_do_caixa);
       printf("%s\n", balcao->inicio_especial->nome);
@@ -225,39 +210,28 @@ void imprime_proximos(Caixa *balcao){
       printf("%s\n", balcao->inicio_normal->nome);
       return;
    }
-
 }
 
-/* Faz todos as filas avancarem, caso tenham cliente(s), de uma unidade */
+/* Faz todas as filas avancarem, caso tenham cliente(s), de uma unidade */
 Cliente *remover(Caixa *balcao){ /* A liberacao da memoria ocorre na funcao "main" */
    Cliente *aux=NULL;
    
    if(balcao->inicio_especial != NULL){ /* Verifica que a fila de clientes especiais nao esta vazia */
-      //printf("Um(a) cliente foi atendido(a) na fila %d.\n", balcao->numero_do_caixa);
       aux=balcao->inicio_especial;
       if(balcao->inicio_especial == balcao->fim_especial){ /* Remocao quando so existe um cliente na fila */
          balcao->inicio_especial = NULL; /* Atualiza  o inicio da fila de clientes */
          balcao->fim_especial = NULL; /* Atualiza  o fim da fila de clientes */
-         //free(aux);
       }
-      else{
-         balcao->inicio_especial = balcao->inicio_especial->prox;
-         //free(aux);
-      }
+      else balcao->inicio_especial = balcao->inicio_especial->prox;
       return aux;
    }
    else if(balcao->inicio_normal != NULL){ /* Verifica se a fila de clientes normais esta vazia */
-      //printf("Um(a) cliente foi atendido(a) na fila %d.\n", balcao->numero_do_caixa);
       aux=balcao->inicio_normal;
       if(balcao->inicio_normal == balcao->fim_normal){ /* Remocao quando so existe um cliente na fila */
          balcao->inicio_normal = NULL; /* Atualiza  o inicio da fila de clientes */
          balcao->fim_normal = NULL; /* Atualiza  o fim da fila de clientes */
-         //free(aux);
       }
-      else{
-         balcao->inicio_normal = balcao->inicio_normal->prox; /* Atualiza  o inicio da fila de clientes */
-         //free(aux);
-      }
+      else balcao->inicio_normal = balcao->inicio_normal->prox; /* Atualiza  o inicio da fila de clientes */
       return aux;
    }
    return NULL; /* Erro ao remover cliente */
@@ -266,19 +240,16 @@ Cliente *remover(Caixa *balcao){ /* A liberacao da memoria ocorre na funcao "mai
 /* Desaloca toda a memoria "preenchida pelos clientes" */
 void desalocar_clientes(Cliente *q){
    Cliente *aux=NULL;
-   
    if(q!=NULL){
       aux=q;
       q=q->prox;
       free(aux);
    }
-   
 }
 
 /* Desaloca toda a memória "preenchida pelos caixas" */
 void desalocar_caixas(Caixa *p){
    Caixa *aux=NULL;
-   
    while(p!=NULL){
       if(p!=NULL){
          aux=p;
@@ -288,7 +259,6 @@ void desalocar_caixas(Caixa *p){
          free(aux); /* Libera o caixa */
       }
    }
-   
 }
 
 int main(){
@@ -299,34 +269,24 @@ int main(){
    
    scanf("%d", &n_caixas); /* Capta o numero de caixas do banco */
    
-   balcoes=cria_caixas(n_caixas); /* "balcoes" aponta para o inicio da fila dos caixas */
-   
-   /*printf("'testando' - numero do servico do primeiro caixa: %d\n", balcoes->numero_do_caixa); // Está funcionando
-   balcoes=balcoes->next;
-   printf("'testando' - numero do servico do segundo caixa: %d\n", balcoes->numero_do_caixa); // Está funcionando
-   */
+   balcoes=cria_caixas(n_caixas); /* "balcoes" aponta para o inicio da "lista ligada de caixas */
    
    do{
       scanf("%d", &opcao);
-      
-      switch(opcao){
+      switch(opcao){ /* Avalia a opcao desejada */
          
-         case 1: /* novo cliente entra no banco */
+         case 1: /* Novo cliente entra no banco */
             scanf(" %s %d %d", nome_lido, &prioridade, &servico);
             aux=insere(nome_lido, servico, prioridade, balcoes);
             if(aux != NULL){ /* Inseriu corretamente */
-               if(prioridade == 1){
+               if(prioridade == 1)
                   printf("O(A) cliente %s entrou na fila %d como cliente especial.\n", nome_lido, servico);
-                  
-               }
-               else if(prioridade == 0){
+               else if(prioridade == 0)
                   printf("O(A) cliente %s entrou na fila %d como cliente normal.\n", nome_lido, servico);
-                  
-               }
             }
             break;
             
-         case 2: /* imprime a fila de clientes em cada caixa */
+         case 2: /* Imprime a fila de clientes em cada caixa */
             aux=balcoes;
             printf("Visao geral dos clientes:\n");
             while(aux != NULL){
@@ -335,7 +295,7 @@ int main(){
             }
             break;
             
-         case 3: /* imprime o próximo cliente a ser atendido em cada caixa */
+         case 3: /* Imprime o próximo cliente a ser atendido em cada caixa */
             aux=balcoes;
             printf("Proximos clientes a serem atendidos:\n");
             while(aux != NULL){
@@ -344,9 +304,8 @@ int main(){
             }
             break;
             
-         case 4: /* novo atendimento em todas as filas nao vazias (próximo cliente) */
+         case 4: /* Novo atendimento em todas as filas nao vazias (próximo cliente) */
             aux=balcoes;
-            //if(aux != NULL) tmp=remover(aux);
             while(aux != NULL){ /* Removeu corretamente */
                tmp=remover(aux);
                if(tmp!=NULL){
@@ -355,12 +314,10 @@ int main(){
                }
                aux=aux->next;
             }
-            
             break;
             
-         
       }
-   } while(opcao != 5); /* encerra expediente */
+   } while(opcao != 5); /* Encerra o expediente */
    printf("Encerrando expediente... Clientes nao atendidos:\n");
    aux=balcoes;
    while(aux != NULL){
