93358726c9b08d97d1fa6909898a5bd9d0d4c5d1
henriquevs.br <henriquevs.br@gmail.com>
2011-05-30 04:23:45 +0000

    Segunda versão do lab06 - submeti e passou corretamente nos testes do Susy; comentarios  e indentação estão corretos

--- mc202/lab06/lab06_ra105063.c
+++ mc202/lab06/lab06_ra105063.c
@@ -1,4 +1,7 @@
-// lab 06
+/* UNICAMP - Universidade Estadual de Campinas
+   MC202 - Estruturas de Dados
+   Aluno: Henrique Vicente Souza - RA: 105063 - Turma D
+   Laboratorio 06 - Filtro da Mediana */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -43,19 +46,20 @@ void imprime_matriz(int **matriz, int largura, int altura){
    }
 }
 
-// Insertion Sort
-void ordena_vetor(int vetor[], int n_contatos){ // A funcao esta funcionando
+// Ordenacao do vetor atraves do algoritmo "Insertion Sort"
+void ordena_vetor(int vetor[], int tamanho){
    int i, j;
-   int aux[1];
-   for(i = 1; i < n_contatos; i++){
-      aux[0] = vetor[i];
-      for(j = i - 1; (j >= 0) && (aux[0] < vetor[j]); j--){
+   int aux;
+   for(i = 1; i < tamanho; i++){
+      aux = vetor[i];
+      for(j = i - 1; (j >= 0) && (aux < vetor[j]); j--){
          vetor[j + 1] = vetor[j];
       }
-      vetor[j + 1] = aux[0];
+      vetor[j + 1] = aux;
    }
 }
 
+// Aplica o filtro da mediana dentro dos limites da matriz
 int mascara(int **matriz, int largura, int altura, int pos_x, int pos_y){
    int v[TAM];
    int i, j, k=0, val;
@@ -63,7 +67,7 @@ int mascara(int **matriz, int largura, int altura, int pos_x, int pos_y){
       if(i>=0 && i<altura){
          for(j=pos_x-3; j<=pos_x+3; j++){
             if(j>=0 && j<largura){
-               v[k]=matriz[i][j];
+               v[k]=matriz[i][j]; // Guarda os valores medianos
                k++;
             }
          }
@@ -72,55 +76,43 @@ int mascara(int **matriz, int largura, int altura, int pos_x, int pos_y){
    
    ordena_vetor(v, k);
    
-   if(k%2==0)
+   if(k%2==0) // vetor com numero par de elementos
       val=(v[(k/2)-1]+v[(k/2)])/2;
    else
-      val=v[k/2];
+      val=v[k/2]; // vetor com numero impar de elementos
    
    return val;
 }
 
+// Execucao
 int main(){
-   char tipo[2];
+   char tipo[2]; // Tipo da imagem
    int largura, altura, val_maximo, i, j;
    char tmp;
    int **matriz_1=NULL, **matriz_2=NULL;
-   int q[5];
-   q[0]=33;
-   q[1]=23;
-   q[2]=3;
-   q[3]=12;
-   q[4]=25;
-   
-   ordena_vetor(q, 5);
-   //printf("%d %d %d %d %d\n", q[0], q[1], q[2], q[3], q[4]);
    
    tmp=getchar();
    tipo[0]=tmp;
-   //printf("%c\n", tipo[0]);
 
    tmp=getchar();
    tipo[1]=tmp;
-   //printf("%c\n", tipo[1]);
    
    tmp=getchar();
    
    tmp=getchar();
-   while(tmp=='#'){
+   while(tmp=='#'){ // Ignora os comentarios que porventura existam
       while(tmp!='\n')
          tmp=getchar();
       tmp=getchar();
    }
    ungetc(tmp, stdin);
    
-   scanf("%d %d", &largura, &altura);
+   scanf("%d %d", &largura, &altura); // Dimensoes da matriz
    
-   scanf("%d", &val_maximo);
+   scanf("%d", &val_maximo); // Escala de cor
    
-   //printf("Largura:%d\nAltura:%d\nValor Maximo da cor da imagem:%d\n", largura, altura, val_maximo);
-   
-   matriz_1=cria_matriz(largura, altura);
-   matriz_2=cria_matriz(largura, altura);
+   matriz_1=cria_matriz(largura, altura); // Matriz de entrada
+   matriz_2=cria_matriz(largura, altura); // Matriz de saida
    
    // Le a matriz da entrada
    for(i=0; i<altura; i++){
@@ -128,8 +120,7 @@ int main(){
          scanf("%d", &matriz_1[i][j]);
    }
    
-   //imprime_matriz(matriz_1, largura, altura);
-   
+   // Aplica o filtro da mediana na matriz de entrada e guarda os valores na matriz de saida
    for(i=0; i<altura; i++)
       for(j=0; j<largura; j++)
          matriz_2[i][j]=mascara(matriz_1, largura, altura, j, i);
@@ -138,7 +129,7 @@ int main(){
    printf("# MC202D: LAB06 - FILTRO DA MEDIANA\n");
    printf("%d %d\n", largura, altura);
    printf("%d\n", val_maximo);
-   imprime_matriz(matriz_2, largura, altura);
+   imprime_matriz(matriz_2, largura, altura); // Imprime a matriz de saida
    
    return 0;
 }
